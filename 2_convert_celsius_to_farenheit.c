d939b3989d8ff802b9eae23d84bd9d230bceaa18
Ivan Seliverstov <i.seliverstov08@gmail.com>
2026-08-29 22:06:09 +0500

    Сделал упражнение 1.3 и вывел заголовок

--- 2_convert_celsius_to_farenheit.c
+++ 2_convert_celsius_to_farenheit.c
@@ -5,13 +5,16 @@ int main(void)
     const int LOWER_LIMIT = 0;
     const int UPPER_LIMIT = 300;
     const int STEP = 20;
+    const char *DEGREES_FAHRENHEIT_SYMB = "°F";
+    const char *DEGREES_CELSIUS_SYMB = "°C";
 
     int degrees_fahrenheit = LOWER_LIMIT;
     float degrees_celsius = 0;
     
+    printf("%3s\t%6s\n", DEGREES_FAHRENHEIT_SYMB, DEGREES_CELSIUS_SYMB);
     while (degrees_fahrenheit <= UPPER_LIMIT)
     {
-        degrees_celsius = 5.0f * (degrees_fahrenheit - 32.0f) / 9.0f;
+        degrees_celsius = (5.0f / 9.0f) * (degrees_fahrenheit - 32.0f);
         printf("%3d\t%6.1f\n", degrees_fahrenheit, degrees_celsius);
         degrees_fahrenheit += STEP;
     }
