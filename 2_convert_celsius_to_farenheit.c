06c63b7766f9648a1afc5cc75c34b7940ef71e17
Ivan Seliverstov <i.seliverstov08@gmail.com>
2026-08-29 22:19:18 +0500

    Закончил упражнение 1.4

--- 2_convert_celsius_to_farenheit.c
+++ 2_convert_celsius_to_farenheit.c
@@ -11,11 +11,11 @@ int main(void)
     int degrees_fahrenheit = LOWER_LIMIT;
     float degrees_celsius = 0;
     
-    printf("%3s\t%6s\n", DEGREES_FAHRENHEIT_SYMB, DEGREES_CELSIUS_SYMB);
+    printf("%6s\t%3s\n", DEGREES_CELSIUS_SYMB, DEGREES_FAHRENHEIT_SYMB);
     while (degrees_fahrenheit <= UPPER_LIMIT)
     {
         degrees_celsius = (5.0f / 9.0f) * (degrees_fahrenheit - 32.0f);
-        printf("%3d\t%6.1f\n", degrees_fahrenheit, degrees_celsius);
+        printf("%6.1f\t%3d\n", degrees_celsius, degrees_fahrenheit);
         degrees_fahrenheit += STEP;
     }
     return 0;
