a0f19af977006abcc8dfd347662cdd084cab4fb9
Ivan Seliverstov <i.seliverstov08@gmail.com>
2026-08-29 22:59:39 +0500

    На изи сделал упражнение 1.5

--- 1_3_for.c
+++ 1_3_for.c
@@ -6,7 +6,7 @@ int main(void)
     const int MIN_VALUE = 0;
     const int MAX_VALUE = 300;
     const int STEP = 20;
-    for (int degrees_fahrenheit = MIN_VALUE; degrees_fahrenheit <= MAX_VALUE; degrees_fahrenheit += STEP)
+    for (int degrees_fahrenheit = MAX_VALUE; degrees_fahrenheit >= MIN_VALUE; degrees_fahrenheit -= STEP)
     {
         /*°С = (5/9) * (°F - 32)*/
         printf("%3d\t%6.1f\n", degrees_fahrenheit, (5.0f / 9.0f) * (degrees_fahrenheit - 32));
