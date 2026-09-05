d817e923b0160f4e149a057ee0df1628ae75f46b
Ivan Seliverstov <i.seliverstov08@gmail.com>
2026-09-06 00:40:49 +0500

    Выполнил упражнение 1.6

--- 1_5_1_Copy_file.c
+++ 1_5_1_Copy_file.c
@@ -3,9 +3,19 @@
 int main(void)
 {
     int c;
+    int is_eof;
     while ((c = getchar()) != EOF)
     {
         putchar(c);
+        is_eof = (c != EOF);
+        printf("EOF = %d\n", is_eof);
     }
+    is_eof = (c != EOF);
+    printf("EOF = %d\n", is_eof);
+    while ((c = getchar()) != EOF)
+    {
+        /* code */
+    }
+    
     return 0;
 }
